2c49e496ef733b2cdcd2fa73104f4cc00f2e434d
xukaiyyds <3353212714@qq.com>
2024-01-18 23:09:37 +0800

    feat: 根据域名续费倒计时显示不同的颜色提醒

--- js/time.js
+++ js/time.js
@@ -11,6 +11,7 @@ function updateCountdown() {
   }
   document.getElementById("print").innerHTML =
     year + " 年 " + month + " 月 " + date + " 日";
+  /* 温馨提示：为了方便，你只需要在 index.html 的 script 标签里更改就可以了。 */
   // let target = new Date("2023/10/16 21:45:32"); // 设置目标时间
   let currentTime = current.getTime();
   let targetTime = target.getTime();
@@ -37,11 +38,22 @@ function updateCountdown() {
   document.getElementById("countdown").innerHTML =
     days + " 天 " + hours + " 小时 " + minutes + " 分钟 " + seconds + " 秒";
 
-  // 当剩余时间为0时，清除定时器
+  // 当剩余时间小于30时
+  if (days < 30) {
+    document.getElementById("countdown").style.padding = "4px";
+    document.getElementById("countdown").style.backgroundColor = "#4772fa";
+  }
+
+  // 当剩余时间小于7时
+  if (days < 7) {
+    document.getElementById("countdown").style.backgroundColor = "#ffb000";
+  }
+
+  // 当剩余时间小于等于0时，清除定时器
   if (remainingTime <= 0) {
     clearInterval(timer);
-    document.getElementById("countdown").innerHTML = "倒计时结束，你该续费啦！";
-    document.getElementById("countdown").style.color = "#F56C6C";
+    document.getElementById("countdown").textContent = "倒计时结束，你该续费啦！";
+    document.getElementById("countdown").style.backgroundColor = "#e03131";
   }
 }
 
