628cd663438324a237bc99da6cd32ed19098df74
xukaiyyds <3353212714@qq.com>
2024-09-04 18:00:34 +0800

    refactor: 优化加载动画的视觉体验

--- js/load.js
+++ js/load.js
@@ -1,5 +1,6 @@
 const loaded = document.querySelector(".loaded");
 const loadText = document.querySelector(".load-text");
+const twinkle = document.querySelector(".twinkle");
 const container = document.querySelector(".container");
 
 let load = 0;
@@ -11,6 +12,9 @@ function blurring() {
   loadText.innerText = `${load}%`;
   loadText.style.opacity = scale(load, 0, 100, 1, 0);
 
+  if (load > 9) {
+    twinkle.classList.add("display-block");
+  }
   if (load > 99) {
     clearInterval(loading);
     loaded.classList.add("display-none");
@@ -19,7 +23,7 @@ function blurring() {
   }
 
   loaded.addEventListener("click", () => {
-    document.querySelector(".twinkle").classList.add("display-none");
+    twinkle.classList.add("display-none");
     load = 90;
   });
 }
