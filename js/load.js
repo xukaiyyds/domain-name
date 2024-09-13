b8ea48d851f5ecf9e6cfafaf3d6fd602864f32bc
xukaiyyds <3353212714@qq.com>
2024-09-14 03:41:09 +0800

    perf: 加载动画仅首次打开才显示

--- js/load.js
+++ js/load.js
@@ -1,37 +1,46 @@
-const loaded = document.querySelector(".loaded");
-const loadText = document.querySelector(".load-text");
-const twinkle = document.querySelector(".twinkle");
-const container = document.querySelector(".container");
+(function () {
+  const loaded = document.querySelector(".loaded");
+  const loadText = document.querySelector(".load-text");
+  const twinkle = document.querySelector(".twinkle");
+  const container = document.querySelector(".container");
+  const loading = setInterval(blurring, 30);
+  const loadStart = sessionStorage.getItem("load");
+  const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+  };
 
-let load = 0;
+  let load = 0;
 
-let loading = setInterval(blurring, 30);
+  function blurring() {
+    load++;
 
-function blurring() {
-  load++;
+    if (load > 0) {
+      loaded.classList.remove("display-none");
+    }
 
-  loadText.innerText = `${load}%`;
-  loadText.style.opacity = scale(load, 0, 100, 1, 0);
-
-  if (load > 20) {
-    twinkle.classList.remove("display-none");
-    loaded.addEventListener("click", () => {
+    if (Boolean(loadStart)) {
       loadComplete();
-    });
+    }
+
+    loadText.innerText = `${load}%`;
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+
+    if (load > 20) {
+      twinkle.classList.remove("display-none");
+      loaded.addEventListener("click", () => {
+        loadComplete();
+      });
+    }
+
+    if (load > 99) {
+      loadComplete();
+    }
   }
 
-  if (load > 99) {
-    loadComplete();
+  function loadComplete() {
+    clearInterval(loading);
+    sessionStorage.setItem("load", "true");
+    loaded.classList.add("display-none");
+    container.classList.remove("display-none");
   }
-}
-
-function loadComplete() {
-  clearInterval(loading);
-  loaded.classList.add("display-none");
-  container.classList.remove("display-none");
-}
-
-// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
-const scale = (num, in_min, in_max, out_min, out_max) => {
-  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-};
+})();
