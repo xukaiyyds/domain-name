9d62fa06eb20fd5d11c678c5e42575aeafebd062
xukaiyyds <3353212714@qq.com>
2024-09-14 03:45:09 +0800

    feat: 添加禁用浅色主题功能

--- js/prompt.js
+++ js/prompt.js
@@ -8,141 +8,8 @@
     position: "topLeft",
     transitionIn: "bounceInRight",
     transitionOut: "fadeOutLeft",
+    theme: "light",
   });
-
-  /* 分享开关 */
-  const shareLinks = document.querySelector(".share-links");
-  const shareComponent = document.querySelector(".share-component");
-  let getShareSwitch = Boolean(Number(localStorage.getItem("sharelinks")));
-
-  if (getShareSwitch) {
-    shareComponent.classList.remove("display-none");
-    shareLinks.title = "关闭分享";
-  } else {
-    shareComponent.classList.add("display-none");
-  }
-
-  shareLinks.onclick = function () {
-    if (shareComponent.classList.contains("display-none")) {
-      iziToast.show({
-        timeout: false,
-        title: "是否开启分享功能？",
-        icon: "iconfont icon-problem",
-        layout: 1,
-        close: false,
-        drag: false,
-        position: "center",
-        transitionIn: "bounceInDown",
-        transitionOut: "fadeOutDown",
-        overlay: true,
-        overlayColor: "rgba(0, 0, 0, 0.8)",
-        theme: "dark",
-        buttons: [
-          [
-            "<button>开启</button>",
-            function (instance, toast) {
-              localStorage.setItem("sharelinks", "1");
-              getShareSwitch = Boolean(
-                Number(localStorage.getItem("sharelinks"))
-              );
-
-              if (getShareSwitch) {
-                shareComponent.classList.remove("display-none");
-                iziToast.show({
-                  title: "已开启",
-                  icon: "iconfont icon-share-open",
-                  color: "yellow",
-                });
-                setTimeout(function () {
-                  iziToast.show({
-                    message: "请在页面左下角进行分享",
-                    icon: "iconfont icon-info-remind",
-                    color: "blue",
-                    transitionIn: "bounceInDown",
-                  });
-                }, 1500);
-                shareLinks.title = "关闭分享";
-              }
-
-              instance.hide(
-                {
-                  transitionOut: "flipOutX",
-                },
-                toast
-              );
-            },
-            true,
-          ],
-          [
-            "<button>取消</button>",
-            function (instance, toast) {
-              instance.hide(
-                {
-                  transitionOut: "fadeOutUp",
-                },
-                toast
-              );
-            },
-          ],
-        ],
-      });
-    } else {
-      iziToast.show({
-        timeout: false,
-        title: "是否关闭分享功能？",
-        icon: "iconfont icon-problem",
-        layout: 1,
-        close: false,
-        drag: false,
-        position: "center",
-        transitionIn: "bounceInDown",
-        transitionOut: "fadeOutDown",
-        overlay: true,
-        overlayColor: "rgba(0, 0, 0, 0.8)",
-        theme: "light",
-        buttons: [
-          [
-            "<button>关闭</button>",
-            function (instance, toast) {
-              localStorage.setItem("sharelinks", "0");
-              getShareSwitch = Boolean(
-                Number(localStorage.getItem("sharelinks"))
-              );
-
-              if (!getShareSwitch) {
-                shareComponent.classList.add("display-none");
-                iziToast.show({
-                  title: "已关闭",
-                  icon: "iconfont icon-share-close",
-                  color: "yellow",
-                });
-                shareLinks.title = "开启分享";
-              }
-
-              instance.hide(
-                {
-                  transitionOut: "flipOutX",
-                },
-                toast
-              );
-            },
-            true,
-          ],
-          [
-            "<button>取消</button>",
-            function (instance, toast) {
-              instance.hide(
-                {
-                  transitionOut: "fadeOutUp",
-                },
-                toast
-              );
-            },
-          ],
-        ],
-      });
-    }
-  };
 
   /* 复制提醒 */
   document.addEventListener("copy", function (e) {
@@ -162,4 +29,370 @@
       });
     }
   });
+
+  /* 导航菜单 */
+  document.querySelector(".nav").addEventListener("click", () => {
+    iziToast.show({
+      timeout: false,
+      title: "个性化配置",
+      titleLineHeight: 22,
+      icon: "iconfont icon-nav-menu",
+      closeOnEscape: false,
+      progressBar: false,
+      drag: false,
+      position: "center",
+      transitionIn: "fadeInDown",
+      transitionOut: "fadeOut",
+      overlay: true,
+      overlayColor: "rgba(0, 0, 0, 0.8)",
+      theme: "dark",
+      buttons: [
+        [
+          "<button class='light-theme'><i class='iconfont icon-light-theme'></i> <span>启用主题</span></button>",
+          function (instance, toast) {},
+        ],
+        [
+          "<button class='share-links'><i class='iconfont icon-share'></i> <span>开启分享</span></button>",
+          function (instance, toast) {},
+        ],
+        [
+          "<button class='full-screen'><i class='iconfont icon-full-screen'></i> <span>全屏</span></button>",
+          function (instance, toast) {
+            instance.hide({ transitionOut: "flipOutX" }, toast, "button");
+          },
+        ],
+        [
+          "<button><i class='iconfont icon-close'></i> 关闭</button>",
+          function (instance, toast) {
+            instance.hide({ transitionOut: "flipOutX" }, toast, "button");
+          },
+        ],
+      ],
+    });
+    ThemeSwitch();
+    shareSwitch();
+    fullScreenSwitch();
+  });
+
+  /* 明暗切换 */
+  const options = {
+    bottom: "1rem",
+    right: "1rem",
+    left: "unset",
+    time: "0s",
+    mixColor: "#fff",
+    backgroundColor: "#fff",
+    buttonColorDark: "#f3f5f8",
+    buttonColorLight: "#151513",
+    saveInCookies: true,
+    label: "🌓",
+    autoMatchOsTheme: true,
+  };
+
+  const darkmode = new Darkmode(options);
+  darkmode.showWidget();
+
+  const darkmodeLayer = document.querySelector(".darkmode-layer");
+  const darkmodeToggle = document.querySelector(".darkmode-toggle");
+  let getDarkmodes = Boolean(localStorage.getItem("darkmodes"));
+
+  if (getDarkmodes) {
+    darkmodeLayer.style.zIndex = 0;
+    darkmodeToggle.style.zIndex = 1;
+  } else {
+    darkmodeLayer.style.zIndex = -1;
+    darkmodeToggle.style.zIndex = -1;
+  }
+
+  switchState();
+  function switchState() {
+    if (darkmode.isActivated()) {
+      darkmodeToggle.textContent = "🌙";
+    } else {
+      darkmodeToggle.textContent = "☀️";
+    }
+  }
+
+  darkmodeToggle.addEventListener("click", function () {
+    switchState();
+  });
+
+  /* 主题开关 */
+  function ThemeSwitch() {
+    const lightTheme = document.querySelector(".light-theme");
+
+    if (getDarkmodes) {
+      lightTheme.children[1].textContent = "禁用主题";
+    } else {
+      lightTheme.children[1].textContent = "启用主题";
+    }
+
+    lightTheme.addEventListener("click", () => {
+      if (darkmodeToggle.style.zIndex === "-1") {
+        iziToast.show({
+          timeout: false,
+          message: "是否启用浅色主题？",
+          icon: "ico-question",
+          layout: 1,
+          balloon: true,
+          close: false,
+          progressBar: false,
+          position: "bottomLeft",
+          transitionIn: "bounceInUp",
+          transitionOut: "fadeOutUp",
+          buttons: [
+            [
+              "<button>启用</button>",
+              function (instance, toast) {
+                localStorage.setItem("darkmodes", "true");
+                getDarkmodes = Boolean(localStorage.getItem("darkmodes"));
+
+                if (getDarkmodes) {
+                  darkmodeLayer.style.zIndex = 0;
+                  darkmodeToggle.style.zIndex = 1;
+                  iziToast.show({
+                    title: "已启用",
+                    icon: "ico-success",
+                    color: "green",
+                  });
+                  setTimeout(function () {
+                    iziToast.show({
+                      message: "请在页面右下角切换主题",
+                      icon: "iconfont icon-info-remind",
+                      color: "blue",
+                      transitionIn: "bounceInDown",
+                    });
+                  }, 1500);
+                  lightTheme.children[1].textContent = "禁用主题";
+                }
+
+                instance.hide(
+                  {
+                    transitionOut: "fadeOutLeft",
+                  },
+                  toast
+                );
+              },
+              true,
+            ],
+            [
+              "<button>取消</button>",
+              function (instance, toast) {
+                instance.hide(
+                  {
+                    transitionOut: "fadeOutDown",
+                  },
+                  toast
+                );
+              },
+            ],
+          ],
+        });
+      } else {
+        if (darkmode.isActivated()) {
+          iziToast.show({
+            timeout: 3000,
+            title: "温馨提示",
+            titleLineHeight: 20,
+            message: "无法禁用！请先切换到深色主题",
+            icon: "ico-warning",
+            color: "yellow",
+          });
+        } else {
+          iziToast.show({
+            timeout: false,
+            message: "是否禁用浅色主题？",
+            icon: "ico-question",
+            layout: 1,
+            balloon: true,
+            close: false,
+            progressBar: false,
+            position: "bottomLeft",
+            transitionIn: "bounceInUp",
+            transitionOut: "fadeOutUp",
+            buttons: [
+              [
+                "<button>禁用</button>",
+                function (instance, toast) {
+                  localStorage.removeItem("darkmodes");
+                  getDarkmodes = Boolean(localStorage.getItem("darkmodes"));
+
+                  if (!getDarkmodes) {
+                    darkmodeLayer.style.zIndex = -1;
+                    darkmodeToggle.style.zIndex = -1;
+                    iziToast.show({
+                      title: "已禁用",
+                      icon: "ico-error",
+                      color: "green",
+                    });
+                    lightTheme.children[1].textContent = "启用主题";
+                  }
+
+                  instance.hide(
+                    {
+                      transitionOut: "fadeOutLeft",
+                    },
+                    toast
+                  );
+                },
+                true,
+              ],
+              [
+                "<button>取消</button>",
+                function (instance, toast) {
+                  instance.hide(
+                    {
+                      transitionOut: "fadeOutDown",
+                    },
+                    toast
+                  );
+                },
+              ],
+            ],
+          });
+        }
+      }
+    });
+  }
+
+  /* 分享开关 */
+  const shareComponent = document.querySelector(".share-component");
+  let getShareLinks = Boolean(Number(localStorage.getItem("sharelinks")));
+
+  getShareSwitch();
+  function getShareSwitch() {
+    if (getShareLinks) {
+      shareComponent.classList.remove("display-none");
+    } else {
+      shareComponent.classList.add("display-none");
+    }
+  }
+
+  function shareSwitch() {
+    const shareLinks = document.querySelector(".share-links");
+
+    getShareSwitch();
+
+    if (getShareLinks) {
+      shareLinks.children[1].textContent = "关闭分享";
+    }
+
+    shareLinks.addEventListener("click", () => {
+      if (shareComponent.classList.contains("display-none")) {
+        iziToast.show({
+          timeout: false,
+          message: "是否开启分享功能？",
+          icon: "ico-question",
+          layout: 1,
+          balloon: true,
+          close: false,
+          progressBar: false,
+          position: "bottomLeft",
+          transitionIn: "bounceInUp",
+          transitionOut: "fadeOutUp",
+          buttons: [
+            [
+              "<button>开启</button>",
+              function (instance, toast) {
+                localStorage.setItem("sharelinks", "1");
+                getShareLinks = Boolean(
+                  Number(localStorage.getItem("sharelinks"))
+                );
+
+                if (getShareLinks) {
+                  shareComponent.classList.remove("display-none");
+                  iziToast.show({
+                    title: "已开启",
+                    icon: "iconfont icon-share-open",
+                    color: "green",
+                  });
+                  setTimeout(function () {
+                    iziToast.show({
+                      message: "请在页面左下角进行分享",
+                      icon: "iconfont icon-info-remind",
+                      color: "blue",
+                      transitionIn: "bounceInDown",
+                    });
+                  }, 1500);
+                  shareLinks.children[1].textContent = "关闭分享";
+                }
+
+                instance.hide(
+                  {
+                    transitionOut: "fadeOutLeft",
+                  },
+                  toast
+                );
+              },
+              true,
+            ],
+            [
+              "<button>取消</button>",
+              function (instance, toast) {
+                instance.hide(
+                  {
+                    transitionOut: "fadeOutDown",
+                  },
+                  toast
+                );
+              },
+            ],
+          ],
+        });
+      } else {
+        iziToast.show({
+          timeout: false,
+          message: "是否关闭分享功能？",
+          icon: "ico-question",
+          layout: 1,
+          balloon: true,
+          close: false,
+          progressBar: false,
+          position: "bottomLeft",
+          transitionIn: "bounceInUp",
+          transitionOut: "fadeOutUp",
+          buttons: [
+            [
+              "<button>关闭</button>",
+              function (instance, toast) {
+                localStorage.setItem("sharelinks", "0");
+                getShareLinks = Boolean(
+                  Number(localStorage.getItem("sharelinks"))
+                );
+
+                if (!getShareLinks) {
+                  shareComponent.classList.add("display-none");
+                  iziToast.show({
+                    title: "已关闭",
+                    icon: "iconfont icon-share-close",
+                    color: "green",
+                  });
+                  shareLinks.children[1].textContent = "开启分享";
+                }
+
+                instance.hide(
+                  {
+                    transitionOut: "fadeOutLeft",
+                  },
+                  toast
+                );
+              },
+              true,
+            ],
+            [
+              "<button>取消</button>",
+              function (instance, toast) {
+                instance.hide(
+                  {
+                    transitionOut: "fadeOutDown",
+                  },
+                  toast
+                );
+              },
+            ],
+          ],
+        });
+      }
+    });
+  }
 })();
--- js/script.js
+++ js/script.js
@@ -1,39 +1,5 @@
-(function () {
-  /* 明暗切换 */
-  const options = {
-    bottom: "1rem",
-    right: "1rem",
-    left: "unset",
-    time: "0s",
-    mixColor: "#fff",
-    backgroundColor: "#fff",
-    buttonColorDark: "#f3f5f8",
-    buttonColorLight: "#151513",
-    saveInCookies: true,
-    label: "🌓",
-    autoMatchOsTheme: true,
-  };
-
-  const darkmode = new Darkmode(options);
-  darkmode.showWidget();
-
-  const darkmodeToggle = document.querySelector(".darkmode-toggle");
-
-  switchState();
-
-  function switchState() {
-    if (darkmode.isActivated()) {
-      darkmodeToggle.textContent = "🌙";
-    } else {
-      darkmodeToggle.textContent = "☀️";
-    }
-  }
-
-  darkmodeToggle.addEventListener("click", function () {
-    switchState();
-  });
-
-  /* 全屏显示 */
+/* 全屏显示 */
+function fullScreenSwitch() {
   const fullScreen = document.querySelector(".full-screen");
 
   fullScreen.addEventListener("click", function () {
@@ -46,9 +12,9 @@
 
   document.addEventListener("fullscreenchange", function () {
     if (document.fullscreenElement) {
-      fullScreen.title = "退出全屏";
+      console.log("进入全屏模式");
     } else {
-      fullScreen.title = "进入全屏";
+      console.log("退出全屏模式");
     }
   });
 
@@ -64,18 +30,18 @@
       document.mozExitFullscreen ||
       document.msExitFullscreen;
   }
+}
 
-  /* 开源不易，以下信息请不要修改哦 */
-  console.log(
-    "%c 顶级国际域名证书 %c Made by xukaiyyds %c",
-    "background: #35495e; padding: 1px; border-radius: 3px 0 0 3px; color: #fff",
-    "background: #41b883; padding: 1px; border-radius: 0 3px 3px 0; color: #fff",
-    "background: transparent"
-  );
-  console.log(
-    "%c GitHub源代码仓库 %c https://github.com/xukaiyyds/domain-name %c",
-    "background: #35495e; padding: 1px; border-radius: 3px 0 0 3px; color: #fff",
-    "background: #41b883; padding: 1px; border-radius: 0 3px 3px 0; color: #fff",
-    "background: transparent"
-  );
-})();
+/* 开源不易，以下信息请不要修改哦 */
+console.log(
+  "%c 顶级国际域名证书 %c Made by xukaiyyds %c",
+  "background: #35495e; padding: 1px; border-radius: 3px 0 0 3px; color: #fff",
+  "background: #41b883; padding: 1px; border-radius: 0 3px 3px 0; color: #fff",
+  "background: transparent"
+);
+console.log(
+  "%c GitHub源代码仓库 %c https://github.com/xukaiyyds/domain-name %c",
+  "background: #35495e; padding: 1px; border-radius: 3px 0 0 3px; color: #fff",
+  "background: #41b883; padding: 1px; border-radius: 0 3px 3px 0; color: #fff",
+  "background: transparent"
+);
